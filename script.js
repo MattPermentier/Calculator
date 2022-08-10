f3619645b2526181a9ae7c1371dd17c64ad50825
Matt <matt.permentier@gmail.com>
2022-08-10 15:51:27 +0200

    array string numbers are set to numbers operators not

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let display = document.getElementById('showSum');
 let buttons = document.getElementById('buttons');
+let calculateBtn = document.getElementById('calculateBtn');
+
+let sum = [];
 
 buttons.addEventListener('click', (e) => {
   let clickedBtn = e.target;
 
-  if (clickedBtn.tagName !== 'BUTTON') {
-    console.log('wrong click');
-  } else {
-    display.innerHTML = clickedBtn.value;
+  if (clickedBtn.classList.contains('num')) {
+    let toNum = parseInt(clickedBtn.value);
+    sum.push(toNum);
+  } else if (!clickedBtn.classList.contains('num')) {
+    sum.push(clickedBtn.value);
   }
+  //   console.log(sum.join(' '));
+  display.innerHTML = sum.join(' ');
+  operate();
 });
+
+function operate() {}
