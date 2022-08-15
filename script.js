b84b979c59468ac51f0e5f49c6fff08566b47b69
Matt <matt.permentier@gmail.com>
2022-08-15 15:29:20 +0200

    code cleanup + added keyboard events

--- script.js
+++ script.js
@@ -1,47 +1,143 @@
 let display = document.getElementById('showSum');
+let resultDisplay = document.getElementById('showResult');
 let buttons = document.getElementById('buttons');
+let resetBtn = document.querySelector('.reset');
+let deleteBtn = document.querySelector('.delete');
 let calculateBtn = document.getElementById('calculateBtn');
 let operatorBtn = document.querySelectorAll('.operator');
 let clickedNum;
 
-// save clicked numbers
-let num = [];
-let currentNum = '';
+// global variable
+let numValue;
 
-//save clicked operators
-let operator = [];
-let currentOperator = '';
+// array that gets the input values when clicked on calculateBtn
+let num = [];
+
+// saves the input values
+let input = '';
 
 // click event on number buttons
 buttons.addEventListener('click', getValue);
+resetBtn.addEventListener('click', reset);
+deleteBtn.addEventListener('click', deleteInput);
+calculateBtn.addEventListener('click', calculate);
 
 // function to ask for the clicked button value
 function getValue(e) {
   clickedNum = e.target;
-  let numValue = clickedNum.value;
+  numValue = clickedNum.value;
 
-  if (clickedNum.classList.contains('num')) {
-    currentNum += numValue;
-
-    if (currentOperator === '') {
-      return;
-    }
-    //save operator(s) to array
-    num.push(currentOperator);
-    currentOperator = '';
-  } else if (clickedNum.classList.contains('operator')) {
-    currentOperator += numValue;
-
-    if (currentNum === '') {
-      return;
-    }
-    // save num to array
-    num.push(currentNum);
-    currentNum = '';
+  if (
+    clickedNum.classList.contains('num') ||
+    clickedNum.classList.contains('operator')
+  ) {
+    input += numValue;
+    display.innerHTML = input;
+  } else {
+    return;
   }
-  console.log(num.join(' '));
 }
 
-calculateBtn.addEventListener('click', () => {
-  console.log(sum);
+// delete the last input
+function deleteInput() {
+  input = input.slice(0, -1);
+  display.innerHTML = input;
+}
+
+// delete the whole sum
+function reset() {
+  input = '';
+  num = [];
+  display.innerHTML = '0';
+  resultDisplay.innerHTML = '';
+}
+
+// calculates the result
+function calculate() {
+  num.push(input);
+  let userInput = `${num.join('')}`;
+  let result = Function('return ' + userInput)();
+  resultDisplay.innerHTML = result;
+  num = [];
+  input = result;
+}
+
+document.addEventListener('keydown', (e) => {
+  let target = e;
+
+  switch (target.key) {
+    case 'Backspace':
+      deleteInput();
+      break;
+    case 'Delete':
+      reset();
+      break;
+    case 'Enter':
+      calculate();
+      break;
+    case '0':
+      input += '0';
+      display.innerHTML = input;
+      break;
+    case '1':
+      input += '1';
+      display.innerHTML = input;
+      break;
+    case '2':
+      input += '2';
+      display.innerHTML = input;
+      break;
+    case '3':
+      input += '3';
+      display.innerHTML = input;
+      break;
+    case '4':
+      input += '4';
+      display.innerHTML = input;
+      break;
+    case '5':
+      input += '5';
+      display.innerHTML = input;
+      break;
+    case '6':
+      input += '6';
+      display.innerHTML = input;
+      break;
+    case '7':
+      input += '7';
+      display.innerHTML = input;
+      break;
+    case '8':
+      input += '8';
+      display.innerHTML = input;
+      break;
+    case '9':
+      input += '9';
+      display.innerHTML = input;
+      break;
+    case '.':
+      input += '.';
+      display.innerHTML = input;
+      break;
+    case '%':
+      input += '%';
+      display.innerHTML = input;
+      break;
+    case '/':
+      input += '/';
+      display.innerHTML = input;
+      break;
+    case '+':
+      input += '+';
+      display.innerHTML = input;
+      break;
+    case '-':
+      input += '-';
+      display.innerHTML = input;
+      break;
+    case 'x':
+      input += '*';
+      display.innerHTML = input;
+      break;
+  }
 });
